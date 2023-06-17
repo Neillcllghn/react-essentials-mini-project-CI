3e293b4034e9a8640a69dd6755d6b1e744385159
Neillcllghn <>
2023-06-17 10:29:58 +0000

    Functional components - a ref

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>
+          <a>My Photos</a>
+          <a>My Illustrations</a>
+          <a>My Painting</a>
+    </div>
+  )
+}
+
+export default Sidebar;
